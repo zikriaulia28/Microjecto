5bcbc2e6d22eb938b3a3cdd943f1721b2e1a5e67
zikriaulia28 <zikriaulia98@gmail.com>
2023-04-25 21:52:01 +0700

    add history

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -3,12 +3,14 @@ import Header from "@/components/header";
 import Footer from "@/components/footer";
 import Aside from "@/components/SideBar";
 import { getProfile } from "@/utils/https/user";
-import { useEffect, useMemo } from "react";
+import { useEffect, useState, useMemo } from "react";
 import { useSelector } from "react-redux";
 import HistoryDashboard from "@/components/Pages/HistoryDashboard";
 import DashbordDiagram from "@/components/Pages/DashboardDiagram";
 import { useDispatch } from "react-redux";
 import { userAction } from "@/redux/slices/auth";
+import Loader from "@/components/Loader";
+import Link from "next/link";
 
 function Dashboard() {
   const dispatch = useDispatch();
@@ -16,12 +18,17 @@ function Dashboard() {
   const userStore = useSelector((state) => state.user);
   const token = userStore.token;
   const userId = userStore.data.id;
+  const [isLoading, setLoading] = useState(true);
+  const [dataBalance, setDataBalance] = useState({});
 
   const fetching = async () => {
+    setLoading(true);
     try {
       const result = await getProfile(token, userId, controller);
       const balance = result.data.data.balance;
+      setDataBalance(result.data.data);
       dispatch(userAction.editBalanceRedux(balance));
+      setLoading(false);
     } catch (error) {
       console.log(error);
     }
@@ -34,22 +41,27 @@ function Dashboard() {
   return (
     <Title title="Dashboard">
       <Header />
-      <main className="flex gap-5 px-4 xl:px-20 py-10 bg-secondary">
+      <main className="flex gap-5 px-4 xl:px-20 py-10 bg-secondary select-none">
         <Aside namePage="dashboard" />
         <section className="flex flex-col gap-5 w-full h-full">
           <div className="flex justify-between bg-primary rounded-lg shadow-xl p-8 w-full xl:w-full">
-            <div className="flex flex-col gap-6">
-              <p className="text-font-secondary-blur">Balance</p>
-              <p className="text-xl font-bold md:text-4xl text-white">
-                Rp.
-                {(userStore.data.balance &&
-                  userStore.data.balance.toLocaleString("id-ID")) ||
-                  0}
-              </p>
-              <p className="text-font-secondary-blur">
-                {userStore.data.phone || "-"}
-              </p>
-            </div>
+            {isLoading ? (
+              <Loader />
+            ) : (
+              <div className="flex flex-col gap-6">
+                <p className="text-font-secondary-blur">Balance</p>
+                <p className="text-xl font-bold md:text-4xl text-white">
+                  Rp.
+                  {(userStore.data.balance &&
+                    userStore.data.balance.toLocaleString("id-ID")) ||
+                    0}
+                </p>
+                <p className="text-font-secondary-blur">
+                  {userStore.data.phone || "-"}
+                </p>
+              </div>
+            )}
+
             <div className="flex flex-col gap-4">
               <button className="flex border bg-white opacity-20 px-7 py-4 gap-3 rounded-xl">
                 <i className="bi bi-arrow-up mr-2"></i>
@@ -84,7 +96,9 @@ function Dashboard() {
             <div className="flex flex-col gap-10 bg-white w-full xl:w-[40%] h-[28.2rem] rounded-xl shadow p-8">
               <div className="flex justify-between">
                 <h2>Transaction History</h2>
-                <p>See All</p>
+                <Link href={"/history"} className="text-primary cursor-pointer">
+                  See All
+                </Link>
               </div>
               <HistoryDashboard token={token} controller={controller} />
             </div>
